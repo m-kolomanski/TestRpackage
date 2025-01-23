2914602c8ed2eaeaf17ae4005453edc49f5cb9e7
m-kolomanski <m.kolomanski@yahoo.com>
2025-01-23 14:45:42 +0100

    added nothelo

---
+++ R/nothello.R
@@ -0,0 +1,18 @@
+# nothello, world!
+#
+# This is an example function named 'nothello' 
+# which prints 'nothello, world!'.
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   https://r-pkgs.org
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Install Package:           'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
+
+nothello <- function() {
+  print("nothello, world!")
+}
